c68c5d0e7a1893e9f4cf96131f274287e99f202f
Matthew Olenik <molenik@versive.com>
2018-05-31 13:52:09 -0700

    FS improvements

--- fs.sh
+++ fs.sh
@@ -1,12 +1,16 @@
+command_exists ag || exitf 1 "This module requires the_silver_searcher"
+
 # Search for files with a valid shebang matching the specified string, e.g.:
 #   find_with_shebang sh
 # Would match files starting with "#!/bin/sh" and "#!/usr/bin/env bash"
 find_with_shebang() {
   # ag automatically respects patterns in .gitignore
   # Also ignore tpl files which may contain templatized shell that can't be linted
-  ag --ignore 'node_modules' --ignore '.git' -l '^#!/.*'"$1" .
+  ag --ignore 'node_modules' --ignore '*.tpl' --ignore '.git' -l '^#!/.*'"$1" .
 }
 
+# Finds files matching one or more file extensions
 find_with_extension() {
-  ag --ignore 'node_modules' --ignore '.git' -l -G "$1" .
+  # The regex that gets built here looks like: .*\.arg1|arg2|arg3$
+  ag --ignore 'node_modules' --ignore '.git' -l -g ".*\\.$(printf "%s" "$*" | sed 's/ /|/g')$" .
 }
